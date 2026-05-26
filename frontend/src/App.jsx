03843b2120ff37b7e94c8d8a8322932bcb19be06
Nader PJR <pjrnader@gmail.com>
2026-05-26 14:00:22 +0430

    login page added for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,45 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { Login } from "./pages/Login";
 import Register from "./pages/Register";
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 function App() {
 
   const [user, setUser] = useState(null);
-  const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/api/auth/me")
+        setUser(res.data);
+      } catch (error) {
+        setUser(null);
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchUser();
+  }, [])
+
+  if (loading) {
+    console.log("Loading user data...");
+    return <div>Loading...</div>;
+  }
 
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,38 @@
+import { useState } from "react"
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
-export const Login = () => {
+export const Login = ({ setUser }) => {
+
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+    })
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post("http://localhost:5000/api/auth/login", form);
+            setUser(res.data);
+            navigate("/");
+
+        } catch (error) {
+            setError("Invalid email or password");
+        }
+    }
+
     return (
-        <div>Login</div>
+        <form className="form" onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            {error && <p className="text-error">{error}</p>}
+            <input type="email" placeholder="email" value={form.email} onChange={(e) => setForm({ ...form, email: e.target.value })} />
+
+            <input type="password" placeholder="password" value={form.password} onChange={(e) => setForm({ ...form, password: e.target.value })} />
+
+            <button>Log in</button>
+
+        </form>
     )
 }
